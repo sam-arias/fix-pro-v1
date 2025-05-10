0d85dc2df2534da6ab47b303a7d305bc35ad228d
Juan-Jose-Avendano-Zapata <juan.avendano27@correo.tdea.edu.co>
2025-05-09 23:19:27 -0500

    Update External Server

--- src/main/java/com/localMantenimiento/fixpro/Config/CorsConfig.java
+++ src/main/java/com/localMantenimiento/fixpro/Config/CorsConfig.java
@@ -18,6 +18,7 @@ public class CorsConfig {
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("")
             .allowCredentials(true);
+
       }
     };
   }
