8305cdc562eff043bebde324afa08feb70035d0a
Juan-Jose-Avendano-Zapata <juan.avendano27@correo.tdea.edu.co>
2025-05-09 23:18:15 -0500

    Update login 2

---
+++ src/main/java/com/localMantenimiento/fixpro/Config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.localMantenimiento.fixpro.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/*") // Aplica a todas las rutas
+            .allowedOrigins("http://127.0.0.1:5500/") // Ajusta si usas otro puerto
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("")
+            .allowCredentials(true);
+      }
+    };
+  }
+}
