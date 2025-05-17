48dbad16fe7709d9cc82b0067e8cea0d9db13fb3
Rockert6402 <148976555+Rockert6402@users.noreply.github.com>
2025-05-16 20:25:47 -0500

    cambios en el tecnico

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -9,7 +9,7 @@ const ROLES_CONFIG = {
   ADMIN: {
       name: 'Administrador',
       color: 'e74a3b',
-      dashboard: 'admin-dashboard.html',
+      dashboard: '/assets/views/admin-dashboard.html',
       level: 3, // Nivel de jerarquía
       menuItems: [
           { icon: 'fa-users', text: 'Usuarios', action: 'showUserManagement' },
@@ -22,7 +22,7 @@ const ROLES_CONFIG = {
   MANAGER: {
       name: 'Gerente',
       color: '1cc88a',
-      dashboard: 'manager-dashboard.html',
+      dashboard: '/assets/views/manager-dashboard.html',
       level: 2,
       menuItems: [
           { icon: 'fa-clipboard-list', text: 'Órdenes', action: 'showAllOrders' },
@@ -34,7 +34,7 @@ const ROLES_CONFIG = {
   TECHNICIAN: {
       name: 'Técnico',
       color: '4e73df',
-      dashboard: 'technician-dashboard.html',
+      dashboard: '/assets/views/technician-dashboard.html',
       level: 1,
       menuItems: [
           { icon: 'fa-tasks', text: 'Mis Órdenes', action: 'showMyOrders' },
